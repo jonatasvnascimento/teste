cb859807ee94bbd37fcc1370f638e8431ad0007b
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-03-25 12:14:53 -0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            readExcel();
+        }
+        public void readExcel()
+        {
+            string path = @"C:\Users\jnascimento3\Desktop\teste\p1.xlsx";
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook wb;
+            Worksheet ws;
+
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[1];
+            MessageBox.Show(Convert.ToString(ws.Cells[1, 1].value));
+        }
+    }
+}
